65f233e672a31183a1b7dc6a87e0937eff43afa2
Roberto Trani <frasitalia@gmail.com>
2019-11-07 17:19:45 +0100

    Fix for gcc-8

--- src/filtering/types.hpp
+++ src/filtering/types.hpp
@@ -1,6 +1,9 @@
 #ifndef FILTERING_TYPES_HPP
 #define FILTERING_TYPES_HPP
 
+#include <cmath>
+
+
 typedef std::float_t score_type;
 typedef std::float_t relevance_type;
 typedef std::uint32_t index_type;
